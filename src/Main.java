0f39feb25e1b6deff1cc88de0a8d513d21326af8
Luis <zoonajluis@gmail.com>
2024-10-09 20:22:09 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("\n\n----------------------------BIENVENIDO A SU CALCULADORA VIRTUAL----------------------------");
+        System.out.println("\n----------Calculo de areas-----------");
+        System.out.println("1) Cuadrado");
+        System.out.println("2)Triangulo");
+        System.out.println("3) Circulo");
+        System.out.println("seleccione una opcion: ");
+        n = sc.nextInt();
+
+    }
+}
